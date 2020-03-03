5574ea97532c1f5f8ddcda85e54e5cf6ce784907
flyingcanoe <flyingcanoe@protonmail.com>
2020-03-03 16:21:36 -0500

    add more test

--- tests/producer_split_at.rs
+++ tests/producer_split_at.rs
@@ -226,6 +226,12 @@ fn enumerate() {
 
 #[test]
 fn step_by() {
+    let v: Vec<_> = (0..10).step_by(2).collect();
+    check(&v, || (0..10).into_par_iter().step_by(2))
+}
+
+#[test]
+fn step_by_unaligned() {
     let v: Vec<_> = (0..10).step_by(3).collect();
     check(&v, || (0..10).into_par_iter().step_by(3))
 }
